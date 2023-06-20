44030cc973104532d6388733de4025cb51a84a9a
Sebastian Palma <57689355+Sebastian1009@users.noreply.github.com>
2023-06-19 20:35:11 -0400

    UPD: Tabla a exportar en MySQL

---
+++ aereopuerto.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-06-2023 a las 02:34:47
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `aereopuerto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `conductores`
+--
+
+CREATE TABLE `conductores` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `vehiculo_marca` text NOT NULL,
+  `vehiculo_modelo` text NOT NULL,
+  `vehiculo_placa` text NOT NULL,
+  `cantidad_puertas` text NOT NULL,
+  `personas_max` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `conductores`
+--
+
+INSERT INTO `conductores` (`id`, `nombre`, `email`, `password`, `vehiculo_marca`, `vehiculo_modelo`, `vehiculo_placa`, `cantidad_puertas`, `personas_max`) VALUES
+(1, 'Sebastian', '@email.com', '123', 'Chevrolet', 'Cavalier', 'XX-XX-XX', '3', '3');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_viaje`
+--
+
+CREATE TABLE `detalle_viaje` (
+  `id` int(11) NOT NULL,
+  `conductor_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `origen` text NOT NULL,
+  `destino` text NOT NULL,
+  `distancia` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `destino` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viajes`
+--
+
+CREATE TABLE `viajes` (
+  `id` int(11) NOT NULL,
+  `destino` int(15) NOT NULL,
+  `cantidad_puertas` int(1) NOT NULL,
+  `tipo_vehiculo` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `conductores`
+--
+ALTER TABLE `conductores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detalle_viaje`
+--
+ALTER TABLE `detalle_viaje`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `conductor_id` (`conductor_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `viajes`
+--
+ALTER TABLE `viajes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `conductores`
+--
+ALTER TABLE `conductores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_viaje`
+--
+ALTER TABLE `detalle_viaje`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `viajes`
+--
+ALTER TABLE `viajes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_viaje`
+--
+ALTER TABLE `detalle_viaje`
+  ADD CONSTRAINT `detalle_viaje_ibfk_1` FOREIGN KEY (`conductor_id`) REFERENCES `conductores` (`id`),
+  ADD CONSTRAINT `detalle_viaje_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
